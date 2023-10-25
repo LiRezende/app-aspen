ec8cf60dfa34a67797315aeb1512972eb3cfdeab
LiRezende <librusamolin@gmail.com>
2023-10-25 11:29:41 -0300

    Initial settngs.

---
+++ src/main/java/dev/returnapp/appaspen/AppAspenApplication.java
@@ -0,0 +1,13 @@
+package dev.returnapp.appaspen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAspenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAspenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/returnapp/appaspen/AppAspenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.returnapp.appaspen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAspenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
