89016853d7ce9fd6227e2cb65f606c576544cc07
LiRezende <librusamolin@gmail.com>
2023-10-25 14:12:36 -0300

    Prduct repository created.

---
+++ src/main/java/dev/returnapp/appaspen/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package dev.returnapp.appaspen.repositories;
+
+import dev.returnapp.appaspen.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
